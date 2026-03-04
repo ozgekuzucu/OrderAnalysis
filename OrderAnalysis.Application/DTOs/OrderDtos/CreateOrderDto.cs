d77b5240de3c55b2ce2decb43b676ca4951f5eda
ozgekuzucu <ozge.zz53@gmail.com>
2026-03-05 01:06:23 +0300

    Added validation to CreateOrderDto and CreateOrderItemDto

--- OrderAnalysis.Application/DTOs/OrderDtos/CreateOrderDto.cs
+++ OrderAnalysis.Application/DTOs/OrderDtos/CreateOrderDto.cs
@@ -1,6 +1,7 @@
 ﻿using OrderAnalysis.Application.DTOs.OrderItemDtos;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,14 @@ namespace OrderAnalysis.Application.DTOs.OrderDtos
 {
 	public class CreateOrderDto
 	{
+		[Required(ErrorMessage = "Platform zorunludur.")]
 		public string Platform { get; set; }
+
+		[Required(ErrorMessage = "Tarih zorunludur.")]
 		public DateTime Tarih { get; set; }
+
+		[Required(ErrorMessage = "En az bir ürün girilmelidir.")]
+		[MinLength(1, ErrorMessage = "En az bir ürün girilmelidir.")]
 		public List<CreateOrderItemDto> Items { get; set; }
 	}
 }
--- OrderAnalysis.Application/DTOs/OrderItemDtos/CreateOrderItemDto.cs
+++ OrderAnalysis.Application/DTOs/OrderItemDtos/CreateOrderItemDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,22 @@ namespace OrderAnalysis.Application.DTOs.OrderItemDtos
 {
 	public class CreateOrderItemDto
 	{
+		[Required(ErrorMessage = "Ürün adı zorunludur.")]
 		public string Urun { get; set; }
+
+		[Range(0.01, double.MaxValue, ErrorMessage = "Alış fiyatı 0' dan büyük olmalıdır")]
 		public decimal AlisFiyat { get; set; }
+
+		[Range(0.01, double.MaxValue, ErrorMessage = "Satış fiyatı 0' dan büyük olmalıdır")]
 		public decimal SatisFiyat { get; set; }
+
+		[Range(0,100,ErrorMessage ="Komisyon oranı 0-100 arasında olmalıdır.")]
 		public decimal KomisyonOrani { get; set; }
+
+		[Range(0,double.MaxValue,ErrorMessage ="Kargo bedeli 0 ya da 0'dan büyük olmalıdır.")]
 		public decimal KargoBedeli { get; set; }
+
+		[Range(1,int.MaxValue,ErrorMessage ="En az 1 adet ürün eklenmelidir")]
 		public int Adet { get; set; }
 	}
 }
