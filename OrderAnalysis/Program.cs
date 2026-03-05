a5a2077b1c5ae872305740f11e9679d1f2dbbf6f
ozgekuzucu <ozge.zz53@gmail.com>
2026-03-05 15:49:37 +0300

    Added Global Exception Handler middleware

--- OrderAnalysis/Program.cs
+++ OrderAnalysis/Program.cs
@@ -28,12 +28,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
-	app.UseMiddleware<GlobalExceptionMiddleware>();
+	
 }
 
 app.UseHttpsRedirection();
